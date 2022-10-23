cceab8759e5752f38a484a9d375f23b9c73edb39
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-10-23 20:38:21 +0700

    feat: add environment file

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
-{
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+module.exports = {
   "development": {
-    "username": "username",
-    "password": null,
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
     "database": "postgres",
     "host": "127.0.0.1",
     "dialect": "postgres"
